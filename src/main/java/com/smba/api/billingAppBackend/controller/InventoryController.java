c125084e78ed6a46fdf85df94815cd4b3b51347c
Kishan <kishankumar.sanapala@gmail.com>
2022-11-28 21:28:03 +0530

    patch call for Quantity update

--- src/main/java/com/smba/api/billingAppBackend/controller/InventoryController.java
+++ src/main/java/com/smba/api/billingAppBackend/controller/InventoryController.java
@@ -1,15 +1,22 @@
 package com.smba.api.billingAppBackend.controller;
 
 import java.util.List;
+
+import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smba.api.billingAppBackend.model.Inventory;
@@ -20,8 +27,11 @@ import com.smba.api.billingAppBackend.service.InventoryService;
 @RestController
 @RequestMapping("/api/productlist")
 public class InventoryController  {
+	
+	@Autowired
 	private InventoryService inventoryService;
 
+	
 	public InventoryController(InventoryService inventoryService) {
 		super();
 		this.inventoryService = inventoryService;
@@ -49,7 +59,9 @@ public class InventoryController  {
 		
 	}
 
-	
-	
-	
+	@PatchMapping(path = "/updateQty", consumes = "application/json")
+	public ResponseEntity<Inventory> updateCustomer(@RequestBody Inventory inventory) {
+	    	return new ResponseEntity<Inventory>(inventoryService.updateInventory(inventory), HttpStatus.OK);
+	    
+	}
 }
